a13e0589a36e0d184341f73194c096662daad268
sbenstewart <sbenstewart@gmail.com>
2018-09-27 20:39:00 +0530

    Initial release

---
+++ app/src/main/java/info/androidhive/activityrecognition/DetectedActivitiesIntentService.java
@@ -0,0 +1,145 @@
+package info.androidhive.activityrecognition;
+
+
+import android.app.AlarmManager;
+import android.app.IntentService;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Build;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DetectedActivitiesIntentService  extends IntentService {
+
+    protected static final String TAG = DetectedActivitiesIntentService.class.getSimpleName();
+
+    public DetectedActivitiesIntentService() {
+        // Use the TAG to name the worker thread.
+        super(TAG);
+    }
+
+    public void MuteAudio(){
+        NotificationManager notificationManager =
+                (NotificationManager) getBaseContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && !notificationManager.isNotificationPolicyAccessGranted()) {
+
+            Intent intent = new Intent(
+                    android.provider.Settings
+                            .ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+
+            startActivity(intent);
+        }
+        AudioManager mAlramMAnager = (AudioManager) getBaseContext().getSystemService(Context.AUDIO_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            mAlramMAnager.adjustStreamVolume(AudioManager.STREAM_NOTIFICATION, AudioManager.ADJUST_MUTE, 0);
+            mAlramMAnager.adjustStreamVolume(AudioManager.STREAM_ALARM, AudioManager.ADJUST_MUTE, 0);
+            mAlramMAnager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_MUTE, 0);
+            mAlramMAnager.adjustStreamVolume(AudioManager.STREAM_RING, AudioManager.ADJUST_MUTE, 0);
+            mAlramMAnager.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_MUTE, 0);
+        } else {
+            mAlramMAnager.setStreamMute(AudioManager.STREAM_NOTIFICATION, true);
+            mAlramMAnager.setStreamMute(AudioManager.STREAM_ALARM, true);
+            mAlramMAnager.setStreamMute(AudioManager.STREAM_MUSIC, true);
+            mAlramMAnager.setStreamMute(AudioManager.STREAM_RING, true);
+            mAlramMAnager.setStreamMute(AudioManager.STREAM_SYSTEM, true);
+        }
+        /*
+        AudioManager am;
+        am = (AudioManager) getBaseContext().getSystemService(Context.AUDIO_SERVICE);
+        am.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+        */
+
+    }
+
+    public void UnMuteAudio(){
+        NotificationManager notificationManager =
+                (NotificationManager) getBaseContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && !notificationManager.isNotificationPolicyAccessGranted()) {
+
+            Intent intent = new Intent(
+                    android.provider.Settings
+                            .ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+
+            startActivity(intent);
+        }
+        AudioManager mAlramMAnager = (AudioManager) getBaseContext().getSystemService(Context.AUDIO_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            mAlramMAnager.adjustStreamVolume(AudioManager.STREAM_NOTIFICATION, AudioManager.ADJUST_UNMUTE, 0);
+            mAlramMAnager.adjustStreamVolume(AudioManager.STREAM_ALARM, AudioManager.ADJUST_UNMUTE, 0);
+            mAlramMAnager.adjustStreamVolume(AudioManager.STREAM_MUSIC, AudioManager.ADJUST_UNMUTE,0);
+            mAlramMAnager.adjustStreamVolume(AudioManager.STREAM_RING, AudioManager.ADJUST_UNMUTE, 0);
+            mAlramMAnager.adjustStreamVolume(AudioManager.STREAM_SYSTEM, AudioManager.ADJUST_UNMUTE, 0);
+        } else {
+            mAlramMAnager.setStreamMute(AudioManager.STREAM_NOTIFICATION, false);
+            mAlramMAnager.setStreamMute(AudioManager.STREAM_ALARM, false);
+            mAlramMAnager.setStreamMute(AudioManager.STREAM_MUSIC, false);
+            mAlramMAnager.setStreamMute(AudioManager.STREAM_RING, false);
+            mAlramMAnager.setStreamMute(AudioManager.STREAM_SYSTEM, false);
+        }
+        /*
+        AudioManager am;
+        am = (AudioManager) getBaseContext().getSystemService(Context.AUDIO_SERVICE);
+        am.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+        */
+
+
+    }
+
+
+
+    /**
+     * it sets the timer to print the counter every x seconds
+     */
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+
+        // Get the list of the probable activities associated with the current state of the
+        // device. Each activity is associated with a confidence level, which is an int between
+        // 0 and 100.
+
+
+        DetectedActivity mostProbableActivity = result.getMostProbableActivity();
+
+        Log.i(TAG, "Detected activity: " + mostProbableActivity.getType() + ", " + mostProbableActivity.getConfidence());
+        broadcastActivity(mostProbableActivity);
+        String activity1, activity2;
+        activity1="In Vehicle";
+        activity2="Still";             //"activity_on_bicycle";
+        if(mostProbableActivity.getType() == DetectedActivity.IN_VEHICLE || mostProbableActivity.getType() == DetectedActivity.STILL)
+        {
+            MuteAudio();
+        }
+        else
+        {
+            UnMuteAudio();
+        }
+
+    }
+
+    private void broadcastActivity(DetectedActivity activity) {
+        Intent intent = new Intent(Constants.BROADCAST_DETECTED_ACTIVITY);
+        intent.putExtra("type", activity.getType());
+        intent.putExtra("confidence", activity.getConfidence());
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }
+}
